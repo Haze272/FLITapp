2c3c4b4dbd689f37f6f3b2918c8d0c706b711b38
Stepan <studiorain27@gmail.com>
2022-05-15 20:14:20 +0300

    Убрана уродская полоска сверху

--- app/src/main/java/com/example/flitapp/MainActivity.java
+++ app/src/main/java/com/example/flitapp/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         binding = MainActivityBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        getSupportActionBar().hide();
 
         replaceFragment(new SearchTasksFragment());
 
